fe478b7f0fbf664ae14f0f86cb2973f452d9df8f
owenear <owenear@gmail.com>
2021-07-01 15:19:47 +0300

    small changes

--- nfstats/nfstats/settings.py
+++ nfstats/nfstats/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-70_u69&xwi0*fx(k$(-1s^3183^!$&a4pj)($ffoql_lbwj8ds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ 'nfstats.example.com' ]
 
